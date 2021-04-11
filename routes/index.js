48ab4684dd56e5d1d4b5e8ee1d56d0053b70a30b
anusu90 <anusu90@gmail.com>
2021-04-11 23:28:33 +0530

    mail api

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,13 @@ router.get('/', function (req, res, next) {
 
 router.post("/", async (req, res) => {
 
-  let sendMailtoMePromise = await sendMailToMe(req.body)
-  let sendMailtoUserPromise = await sendMailToUser(req.body.email)
-
-  console.log(sendMailtoMePromise, sendMailtoUserPromise)
-
-
+  try {
+    let sendMailtoMePromise = await sendMailToMe(req.body)
+    let sendMailtoUserPromise = await sendMailToUser(req.body.email)
+    res.status(200).json({ message: "Mail Sent" })
+  } catch (error) {
+    res.status(500).json({ message: "Mail not sent" })
+  }
 })
 
 module.exports = router;
